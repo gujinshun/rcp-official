9a28dea1afe1a108cc8e35a6f1dfd45e338936c4
Gringe920 <e-dam@163.com>
2019-11-13 22:33:56 +0800

    团队介绍

--- src/views/team-intro.vue
+++ src/views/team-intro.vue
@@ -1,79 +1,81 @@
 <template>
   <div class="container">
-    <div class="title inner-title-border">
-        团队介绍
-    </div>
+    <div class="title inner-title-border">团队介绍</div>
     <div class="team-item-list">
-        
+      <div class="arrow">
+          <img src="./../assets/images/team_left.png" alt="">
+      </div>
+      <div class="item-box">
+        <img src="./../assets/images/photo_1.png" alt />
+      </div>
+      <div
+        class="item-box-text"
+      >约翰·纳特尔斯 John Nettles，美国著名经济学家，芝加哥大学教授、芝加哥经济学派领军人物、货币学派的代表人物，1976年诺贝尔经济学奖得主、1951年约翰·贝茨·克拉克奖得主。 弗里德曼被广泛誉为二十世纪最具影响力的经济学家及学者之一。以研究宏观经济学、微观经济学、经济史、统计学、主张自由放任资本主义而闻名。</div>
+      <div class="item-box">
+        <img src="./../assets/images/photo_2.png" alt />
+      </div>
+      <div class="arrow">
+          <img src="./../assets/images/team_right.png" alt="">
+      </div>
     </div>
   </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                itemList: [{
-                    name: '解决方案',
-                    detail: '基于以上对区块链行业现状的分析，ALD将提出结合自身公链优势的合理解决方案',
-                },{
-                    name: '共识算法对比',
-                    detail: '依靠人工神经网络 &遗传算法寻找清 算和支付的最优解',
-                },{
-                    name: '创世节点',
-                    detail: '全球创世节点，共48个，为ALD区块链网络提供初始算力',
-                },{
-                    name: '多功能生态平台',
-                    detail: '全球范围的金融数据化、平台产业化，不限于互联网娱乐、时尚奢侈品、电子金融、实体行业等',
-                },{
-                    name: '安全、逻辑、稳定的系统架构',
-                    detail: '为了解决区块链技术在应用落地过程中可能面临的 各种阻碍，ALD区块链平台采用两层结构',
-                },{
-                    name: '高融合性拓展',
-                    detail: '跨境支付、全球贸易、供应链金融、征信、人工智能\物联网、医疗健康、IP版权、等超广泛的应用场景',
-                }]
-            }
-        },
-        mounted (){
-        },
-        methods : {
-        }
-    }
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
 </script>
 
 <style lang="scss">
-.container{
-    background-image: url('./../assets/images/home_technology_bj.png');
-    background-size: 100% 100%;
+.container {
+  background-image: url("./../assets/images/home_technology_bj.png");
+  background-size: 100% 100%;
+  width: 100%;
+  padding-left: 466px;
+  text-align: left;
+
+  .title {
+    margin-top: 132px;
+    display: inline-block;
+    margin-bottom: 32px;
+  }
+  .team-item-list {
+    display: flex;
+    position: relative;
     width: 100%;
-    padding-left: 466px;
-    text-align: left;
-    
-    .title{
-        margin-top: 132px;
-        display: inline-block;
-        margin-bottom: 32px;
-    }
-    .tech-item-list{
-        display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    .item-box {
+      margin: 9px;
+      img {
         width: 100%;
-        flex-wrap: wrap;
-        .tech-item{
-            width: 25%;
-            text-align: center;
-            margin-right: 8%;
-            margin-bottom: 30px;
-            img{
-                width: 70px;
-                margin-bottom: 15px;
-            }
-            .item-title{
-                font-size: 14px;
-                margin-bottom: 6px;
-            }
-            .item-detail{
-                font-size: 12px;
-            }
-        }
+      }
+      width: 158px;
+      height: 216px;
     }
+    .item-box-text {
+      margin: 9px;
+      width: 158px;
+      height: 216px;
+      background: rgba(2, 35, 2, 1);
+      opacity: 0.7;
+      font-size: 2px;
+      color: white;
+      padding: 5px;
+    }
+    .arrow {
+        cursor: pointer;
+        margin: 9px;
+        img {
+            width: 100%;
+        }
+        width: 27px;
+        height: 27px;
+    }
+  }
 }
 </style>
